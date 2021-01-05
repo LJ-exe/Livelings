f3b6c1eb65e9c616548aafd53407cc33f091a5b5
Lukas <ljgaming4l4@gmail.com>
2021-01-05 09:57:31 +0000

    Added main.py and added a starter / info msg

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+print("""Welcome to livelings
+This is a game where you collect livelings and battle them against the A.I
+To start go to the readme.txt file linked in the repo and see all the commands you can use
+Try typing 'start' to get your first liveling
+""")
+
+class liveling:
+    
